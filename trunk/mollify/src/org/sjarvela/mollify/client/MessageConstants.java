0c626a40876b22bf82cd9659471b4c92ca0cdb2a
samuli.jarvela <samuli.jarvela@2994f5a1-a855-0410-a0c2-a9f3307cd5ac>
2008-08-30 17:07:08 +0000

    Added EPL license text

--- trunk/mollify/src/org/sjarvela/mollify/client/MessageConstants.java
+++ trunk/mollify/src/org/sjarvela/mollify/client/MessageConstants.java
@@ -1,11 +1,25 @@
+/**
+ * Copyright (c) 2008- Samuli Järvelä
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
+ * this entire header must remain intact.
+ */
+
 package org.sjarvela.mollify.client;
 
 import com.google.gwt.i18n.client.Messages;
 
-public interface MessageConstants extends Messages{
+public interface MessageConstants extends Messages {
 	String sizeOneByte();
+
 	String sizeInBytes(int bytes);
+
 	String sizeOneKilobyte();
+
 	String sizeInKilobytes(double kilobytes);
+
 	String sizeInMegabytes(double megabytes);
 }
