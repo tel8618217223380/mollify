87831d142e4d099828e2f9777b0f3f0a30367de4
samuli.jarvela <samuli.jarvela@2994f5a1-a855-0410-a0c2-a9f3307cd5ac>
2009-03-17 18:40:26 +0000

    Finalized MySQL installer

--- mollify/backend/install/installation_mysql.php
+++ mollify/backend/install/installation_mysql.php
@@ -1,4 +1,5 @@
 <?php
+
 	/**
 	 * Copyright (c) 2008- Samuli Järvelä
 	 *
@@ -8,54 +9,204 @@
 	 * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
 	 * this entire header must remain intact.
 	 */
-	 
-	function check_configuration_provider() {
+	
+	function check_configuration() {
+		if (!file_exists("../configuration.php")) {
+			error("Configuration does not exist");
+			require("instruction_configuration_mysql.php");
+			return FALSE;
+		}
+		
 		global $CONFIGURATION_PROVIDER;
 		
 		if (!isset($CONFIGURATION_PROVIDER)) {
-			error("Configuration provider not set in <code>configuration.php</code>, <code>\"mysql\"</code> expected.");
+			error("Configuration provider is not defined");
+			require("instruction_configuration_mysql.php");
 			return FALSE;
 		}
 		
 		if (trim(strtolower($CONFIGURATION_PROVIDER)) != 'mysql') {
-			error("Incorrect configuration provider set (<code>\"$CONFIGURATION_PROVIDER\"</code>) in <code>configuration.php</code>, <code>\"mysql\"</code> expected.");
+			error("Incorrect configuration provider defined (<code>\"$CONFIGURATION_PROVIDER\"</code>)");
+			require("instruction_configuration_mysql.php");
 			return FALSE;
 		}
-		
 		return TRUE;
 	}
 	
-	function check_requirements() {
-		print "<h2>Checking requirements...</h2>";
-		print "<span class='item'>Checking MySQL...</span>";
-		if (!check_mysql()) return;
-		print "<span class='item'>Checking Mollify configuration...</span>";
-		if (!check_configuration_provider()) return;
-		if (!check_db_configuration()) return;
-?>
-
-<span class="confirmation">Are you sure you want to install Mollify into MySQL database with information above?
-	<form method="post">
-		<input type="hidden" name="type" value="mysql">
-		<input type="submit" name="install" value="Install">
-	</form>
-</span>
-
-<?php
+	function get_db() {
+		$DEFAULT_HOST = 'localhost';
+		$DEFAULT_DB = 'mollify';
+		$db = get_db_configuration();
+		
+		if ($db["user"] === NULL) {
+			error("Database user not defined");
+			require("instruction_configuration_mysql.php");
+			return FALSE;
+		}
+		if ($db["password"] === NULL) {
+			error("Database password not defined");
+			require("instruction_configuration_mysql.php");
+			return FALSE;
+		}
+		
+		$db["host_defined"] = TRUE;
+		if ($db["host"] === NULL) {
+			$db["host"] = $DEFAULT_HOST;
+			$db["host_defined"] = FALSE;
+		}
+		
+		$db["database_defined"] = TRUE;
+		if ($db["database"] === NULL) {
+			$db["database"] = $DEFAULT_DB;
+			$db["database_defined"] = FALSE;
+		}
+				
+		return $db;
 	}
 	
-	function install() {
-		print "<h2>Installing...</h2>";
-		if (!check_permissions()) return;
+	function check_installed($db) {
+		$version = get_installed_version($db);
+		if (!$version) return TRUE;
+		
+		error("Mollify already installed");
+		return FALSE;
+	}
+
+	function print_db($db) {		
+		print "<div class='list'><div class='title'>Database information</div>";
+		
+		if (!$db["host_defined"]) {
+			list_item("Database host:", "<code>\"".$db["host"]."\"</code> (default)");
+		} else {
+			list_item("Database host:", "<code>\"".$db["host"]."\"</code>");
+		}
+		
+		if (!$db["database_defined"]) {
+			list_item("Database name:", "<code>\"".$db["database"]."\"</code> (default)");
+		} else {
+			list_item("Database name:", "<code>\"".$db["database"]."\"</code>");
+		}
+		
+		list_item("Database user:", "<code>\"".$db['user']."\"</code>");
+		
+		print "</div>";
+	}
+	
+	function list_item($name, $value) {
+		print("<div class='list-item'><div class='name'>$name</div><div class='value'>$value</div></div>");
+	}
+	
+	function step_database() {
+		print "<h2>1/3 Database Configuration</h2>";
+
+		if (!check_configuration()) return;
+		if (!check_mysql()) return;
+		if (!($db = get_db())) return;
+		if (!check_installed($db)) return;
+		
+		print_db($db);
+		
+		if (!check_connection($db)) {
+			error("Cannot connect to database");
+			return FALSE;
+		}
+		if (!($db_exists = check_database($db))) {
+			print("<span class='note'>Database \"".$db["database"]."\" does not exist. If you continue with installation, it will be created.</span>");
+		} 
+
+		print "<span class='confirmation'>";
+		print "<form method='post'>";
+		print "<span class='message'>Continue with this configuration?</span>";
+		print "<input type='hidden' name='type' value='mysql'>";
+		print "<input type='hidden' name='step' value='configuration'>";
+		if (!$db_exists) print "<input type='hidden' name='create_db' value='1'>";
+		print "<input type='submit' name='action' value='Continue'>";
+		print "</form>";
+		print "</span>";
+	}
+
+	function step_configuration() {
+		if (!($db = get_db())) return;
+		
+		if (isset($_POST["create_db"]) and !create_database($db)) {
+			print "<h2>1/3 Database Configuration</h2>";
+			error("Could not create database. Try creating database manually and restarting installation.");
+			print_db_error();
+			print_db($db);
+			return FALSE;
+		}
+				
+		if (($failed = check_db_permissions($db)) != NULL) {
+			print "<h2>1/3 Database Configuration</h2>";
+			error("Database permission test failed, could not ".$failed.". Check database user settings and try again.");
+			print_db_error();
+			print_db($db);
+			return FALSE;
+		}
+		
+		print "<h2>2/3 Mollify Configuration</h2>";
+		print "<span class='user'>";
+		print "<form method='post'>";
+		print "<span class='title'>Enter Mollify admin user information:</span>";
+		print "<div class='user_data' id='user'><div class='title'>User name:</div><input class='value' type='text' name='user' value=''></div>";
+		print "<div class='user_data' id='password'><div class='title'>Password:</div><input class='value' type='password' name='password' value=''></div>";
+		print "</span>";
+		print "<span class='confirmation'>";
+		print "<span class='message'>Continue to installation?</span>";
+		print "<input type='hidden' name='type' value='mysql'>";
+		print "<input type='hidden' name='step' value='install'>";
+		print "<input type='submit' name='action' value='Install Mollify'>";
+		print "</span>";
+		print "</form>";
+	}
+	
+	function print_db_error($error) {
+		print "<span class='db-error'>".$error["no"].": ".$error["error"]."</span>";
+	}
+	
+	function step_install() {
+		if (!($db = get_db())) return;
+
+		if (!isset($_POST["user"]) or strlen($_POST["user"]) === 0 or !isset($_POST["password"]) or strlen($_POST["password"]) === 0) {
+			print "<h2>2/3 Mollify Configuration</h2>";
+			error("Missing admin user information.");
+			return FALSE;
+		}
+		if ($error = create_tables($db)) {
+			print "<h2>2/3 Mollify Configuration</h2>";
+			error("Could not create Mollify tables.");
+			print_db_error($error);
+			print_db($db);
+			return FALSE;
+		}
+		if ($error = insert_admin_user($db, $_POST["user"], $_POST["password"])) {
+			print "<h2>2/3 Mollify Configuration</h2>";
+			error("Could not create Mollify user.");
+			print_db_error($error);
+			print_db($db);
+			return FALSE;
+		}
+		if ($error = insert_param($db, "version", "100")) {
+			print "<h2>2/3 Mollify Configuration</h2>";
+			error("Failed to update Mollify version data.");
+			print_db_error($error);
+			print_db($db);
+			return FALSE;
+		}
+		
+		print "<h2>3/3 Installation</h2>";
+		info("Mollify successfully installed!");
 	}
 
 	function on_page() {
-		require_once("mysql.php");
-		
-		if (isset($_POST["start"])) {
-			check_requirements();
-		} else if (isset($_POST["install"])) {
-			install();
+		require("mysql.php");
+				
+		if (!isset($_POST["step"])) {
+			step_database();
+		} else if ($_POST["step"] === 'configuration') {
+			step_configuration();
+		} else if ($_POST["step"] === 'install') {
+			step_install();
 		} else {
 			error("Error in installer script.");
 		}
--- mollify/backend/install/mysql.php
+++ mollify/backend/install/mysql.php
@@ -1,4 +1,5 @@
 <?php
+
 	/**
 	 * Copyright (c) 2008- Samuli Järvelä
 	 *
@@ -8,7 +9,7 @@
 	 * http://www.eclipse.org/legal/epl-v10.html. If redistributing this code,
 	 * this entire header must remain intact.
 	 */
-	 
+
 	function check_mysql() {
 		if (!function_exists('mysql_connect')) {
 			error("MySQL not detected in the system, check system configuration.");
@@ -17,116 +18,175 @@
 		return TRUE;
 	}
 
-	function check_installation() {
-		$conf = get_db_configuration();
-		$connection = @mysql_connect($conf['host'], $conf['user'], $conf['password'], TRUE, 2);
-		if (!$connection) return TRUE;
-		if (!mysql_select_db($conf['db'])) return TRUE;
+	function get_installed_version($db) {
+		$connection = @mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return FALSE;
+		if (!mysql_select_db($db['database'])) return FALSE;
 		
 		$result = mysql_query("SELECT value FROM param WHERE name='version'");
-		if (mysql_error() or mysql_num_rows($result) === 0) return TRUE;
+		if (mysql_error() or mysql_num_rows($result) === 0) return FALSE;
 		
-		$version = mysql_result($result);
-		if (trim($version) === "") return TRUE;
+		$version = mysql_result($result, 0);
+		if (trim($version) === "") return FALSE;
 
-		error("Mollify already installed (version ".$version.")");
-		return FALSE;
-	}
-	
-	function check_db_configuration() {
-		global $DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE;
-		
-		if (!isset($DB_USER)) {
-			error("Database user not found in configuration.");
-			return FALSE;
-		}
-
-		if (!isset($DB_PASSWORD)) {
-			error("Database password not found in configuration.");
-			return FALSE;
-		}
-		
-		print "<span class='title'>Database information</span>";
-		
-		if (!isset($DB_HOST)) info("Database host: No host found in configuration, assuming <code>localhost</code>.");
-		else info("Database host: ".$DB_HOST);
-		
-		if (!isset($DB_DATABASE)) info("Database name: No database name found in configuration, using default (<code>\"mollify\"</code>).");
-		else info("Database name: ".$DB_DATABASE);
-		
-		info("Database user: ".$DB_USER);
-		
-		return TRUE;
+		return $version;
 	}
 	
 	function get_db_configuration() {
 		global $DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE;
 		
-		if (isset($DB_HOST)) $host = $DB_HOST;
-		else $host = "localhost";
+		$result = array();
+
+		$result["host"] = NULL;
+		if (isset($DB_HOST)) $result["host"] = $DB_HOST;
 		
-		if (isset($DB_DATABASE)) $database = $DB_DATABASE;
-		else $database = "mollify";
-		
-		return array("host" => $host, "db" => $database, "user" => $DB_USER, "password" => $DB_PASSWORD);		
+		$result["database"] = NULL;
+		if (isset($DB_DATABASE)) $result["database"] = $DB_DATABASE;
+
+		$result["user"] = NULL;
+		if (isset($DB_USER)) $result["user"] = $DB_USER;
+
+		$result["password"] = NULL;
+		if (isset($DB_PASSWORD)) $result["password"] = $DB_PASSWORD;
+				
+		return $result;
 	}
 	
-	function check_permissions() {
-		$conf = get_db_configuration();
-		$connection = @mysql_connect($conf['host'], $conf['user'], $conf['password'], TRUE, 2);
-		if (!$connection) {
-			error("Could not connect to database, error: <code>".mysql_error()."</code>");
-			return FALSE;
-		}
-
-		if (!mysql_select_db($conf['db'])) {
-			if (!mysql_query("CREATE DATABASE ".$conf['db']."")) {
-				error("Could not create database, error: <code>".mysql_error()."</code>");
-				return FALSE;
-			}
-			
-			if (!mysql_select_db($conf['db'])) { 
-				error("Could not select database, error: <code>".mysql_error()."</code>");
-				return FALSE;
-			}
-		}
-
-		$query = 'CREATE TABLE mollify_install_test (id int NULL)';
-		$result = mysql_query($query);
-		if ($error = mysql_error()) {
-			error("Could not create database table, error: <code>".$error."</code>");
-			return FALSE;
-		}
-
-		$query = 'INSERT INTO mollify_install_test (id) VALUES (1)';
-		$result = mysql_query($query);
-		if ($error = mysql_error()) {
-			error("Could not insert data into database table, error: <code>".$error."</code>");
-			return FALSE;
-		}
-
-		$query = 'UPDATE mollify_install_test SET id = 2';
-		$result = mysql_query($query);
-		if ($error = mysql_error()) {
-			error("Could not update data into database table, error: <code>".$error."</code>");
-			return FALSE;
-		}
-
-		$query = 'DELETE FROM mollify_install_test';
-		$result = mysql_query($query);
-		if ($error = mysql_error()) {
-			error("Could not delete data from database table, error: <code>".$error."</code>");
-			return FALSE;
-		}
-		
-		$query = 'DROP TABLE mollify_install_test';
-		$result = mysql_query($query);
-		if ($error = mysql_error()) {
-			error("Could not drop database table, error: <code>".$error."</code>");
-			return FALSE;
-		}
-
+	function db_error($msg, $error) {
+		error("<span class='error'>$msg<span class='details'>$error</span></span>");
+	}
+	
+	function check_connection($db) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return FALSE;
 		mysql_close($connection);
 		return TRUE;
+	}
+	
+	function check_database($db) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return FALSE;
+
+		$result = mysql_select_db($db['database']);
+		mysql_close($connection);
+		return $result;		
+	}
+		
+	function create_database($db) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return FALSE;
+			
+		$result = mysql_query("CREATE DATABASE ".$db['database']."");		
+		if ($result) $result = mysql_select_db($db['database']);
+		
+		mysql_close($connection);
+		return $result;
+	}
+	
+	function check_db_permissions($db) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return "connect";
+		
+		if (!mysql_select_db($db['database'])) {
+			mysql_close($connection);
+			return "select database";
+		}
+		
+		$tests = array("create table" => 'CREATE TABLE mollify_install_test (id int NULL)',
+			"insert data" => 'INSERT INTO mollify_install_test (id) VALUES (1)',
+			"update data" => 'UPDATE mollify_install_test SET id = 2',
+			"delete data" => 'DELETE FROM mollify_install_test',
+			"drop table" => 'DROP TABLE mollify_install_test');
+			
+		foreach ($tests as $name => $query) {
+			if (!mysql_query($query)) {
+				mysql_close($connection);
+				return $name;
+			}
+		}
+		
+		mysql_close($connection);
+		return NULL;
+	}
+	
+	function _query($db, $query) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return array("no" => mysql_errno(), "error" => mysql_error());
+		
+		$result = mysql_select_db($db['database']);
+		if (!$result) {
+			$result = array("no" => mysql_errno(), "error" => mysql_error());
+		} else {	
+			$result = mysql_query($query);		
+			if (!$result) $result = array("no" => mysql_errno(), "error" => mysql_error());
+			else $result = NULL;
+		}
+		
+		mysql_close($connection);
+		return $result;
+	}
+
+	function _queries($db, $queries) {
+		$connection = mysql_connect($db['host'], $db['user'], $db['password'], TRUE, 2);
+		if (!$connection) return array("no" => mysql_errno(), "error" => mysql_error());
+		
+		$result = mysql_select_db($db['database']);
+		if (!$result) {
+			$result = array("no" => mysql_errno(), "error" => mysql_error());
+		} else {
+			foreach($queries as $query) {
+				$result = mysql_query($query);
+				if (!$result) break;
+			}
+			
+			if (!$result) $result = array("no" => mysql_errno(), "error" => mysql_error());
+			else $result = NULL;
+		}
+		
+		mysql_close($connection);
+		return $result;
+	}
+		
+	function create_tables($db) {
+		$tables = array("
+CREATE TABLE `folder` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
+","
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `permission_mode` char(2) character set latin1 NOT NULL,
+  PRIMARY KEY  (`id`)
+);
+","
+CREATE TABLE `user_folder` (
+  `user_id` int(11) NOT NULL,
+  `folder_id` int(11) NOT NULL,
+  PRIMARY KEY  (`user_id`,`folder_id`),
+  KEY `fk_pf_folder` (`folder_id`)
+);
+","
+CREATE TABLE `param` (
+  `name` char(255) character set latin1 NOT NULL,
+  `value` char(255) character set latin1 NOT NULL,
+  PRIMARY KEY  (`name`)
+);
+");
+		return _queries($db, $tables);
+	}
+	
+	function insert_admin_user($db, $user, $pw) {
+		$query = "INSERT INTO user (name, password, permission_mode) VALUES ('".mysql_escape_string($user)."','".md5($pw)."','A')";
+		return _query($db, $query);
+	}
+	
+	function insert_param($db, $name, $value) {
+		$query = "INSERT INTO param (name, value) VALUES ('".mysql_escape_string($name)."','".mysql_escape_string($value)."')";
+		return _query($db, $query);
 	}
 ?>
