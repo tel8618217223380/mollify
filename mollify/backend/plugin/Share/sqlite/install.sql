6fa69ad040cceaf22e68e1a16da98ecf3d073e26
samuli.jarvela <samuli.jarvela@gmail.com>
2012-07-12 19:33:02 +0000

    Share plugin sqlite error

--- mollify/backend/plugin/Share/sqlite/install.sql
+++ mollify/backend/plugin/Share/sqlite/install.sql
@@ -3,8 +3,8 @@ CREATE TABLE share (
   item_id char(255) NOT NULL,
   name varchar(255) NOT NULL,
   user_id INTEGER NOT NULL,
-  expiration bigint(11) NOT NULL,
+  expiration bigint(11) NULL,
   created bigint(11) NOT NULL,
   active TINYINT(1) NOT NULL
 );
-INSERT INTO parameter (name, value) VALUES ('plugin_Share_version', '1_1');
+INSERT INTO parameter (name, value) VALUES ('plugin_Share_version', '1_2');
