0461b54158cbe545be5a1f221bcd4169697747e6
samuli.jarvela <samuli.jarvela@gmail.com>
2012-07-25 19:10:35 +0000

    apache_request_headers & MySQLI lastId

--- mollify/backend/include/Request.class.php
+++ mollify/backend/include/Request.class.php
@@ -156,4 +156,27 @@
 			return $this->data;
 		}
 	}
+	
+	// emulate "apache_request_headers" if it does not exist
+	if (!function_exists('apache_request_headers')) {
+		function apache_request_headers() {
+			$arh = array();
+			$rx_http = '/\AHTTP_/';
+			
+			foreach($_SERVER as $key => $val) {
+				if (preg_match($rx_http, $key)) {
+					$arh_key = preg_replace($rx_http, '', $key);
+	
+					$rx_matches = explode('_', $arh_key);
+					if (count($rx_matches) > 0 and strlen($arh_key) > 2) {
+			      		foreach ($rx_matches as $ak_key => $ak_val)
+			      			$rx_matches[$ak_key] = ucfirst($ak_val);
+			      		$arh_key = implode('-', $rx_matches);
+			      	}
+			      	$arh[$arh_key] = $val;
+			    }
+			}
+			return $arh;
+		}
+	}
 ?>
