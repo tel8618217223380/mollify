6a063617ad6a1c379ffec4b67ddeb2ead1eb339c
samuli.jarvela <samuli.jarvela@gmail.com>
2012-11-06 16:13:13 +0000

    ItemCollection

--- mollify/backend/plugin/ItemCollection/pages/empty.php
+++ mollify/backend/plugin/ItemCollection/pages/empty.php
@@ -1,16 +1,6 @@
 <html>
 	<head>
 		<script type="text/javascript" language="javascript" src="<?php print $resourcesUrl ?>/jquery-1.4.2.min.js"></script>
-		<style>
-			#itemcollection-preparing, #itemcollection-downloading {
-				font-size: 31.5px;
-				margin: 20px;
-				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-				font-weight: bold;
-				line-height: 40px;
-				text-rendering: optimizelegibility;
-			}
-		</style>
 		<script>
 			$(document).ready(function() {
 				var loc = window.location;
@@ -23,11 +13,8 @@
 		</script>
 	</head>
 	<body>
-		<div id="itemcollection-preparing">
-		Preparing...
-		</div>
-		<div id="itemcollection-downloading" style="display:none">
-		Downloading...
+		<div id="itemcollection-empty">
+		The collection you requested is empty.
 		</div>
 	</body>
 </html>
