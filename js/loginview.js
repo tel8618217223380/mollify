b6f96bb8e737411641fd2b712ee6ae84ea181a2a
samuli.jarvela <samuli.jarvela@gmail.com>
2013-04-20 18:09:23 +0000

    new structure

---
+++ js/loginview.js
@@ -0,0 +1,106 @@
+!function($, mollify) {
+
+	"use strict"; // jshint ;_;
+	
+	mollify.view.LoginView = function(){
+		var that = this;
+		
+		that.init = function($c) {
+			mollify.dom.loadContentInto($c, mollify.templates.url("loginview.html"), that, ['localize', 'bubble']);
+		}
+		
+		that.onLoad = function() {
+			$(window).resize(that.onResize);
+			that.onResize();
+		
+			if (mollify.features.hasFeature('lost_password')) $("#login-lost-password").show();
+			if (mollify.features.hasFeature('registration')) {
+				$("#mollify-login-register").click(function() {
+					mollify.ui.window.open(mollify.service.pluginUrl("registration"));
+				});
+				$("#mollify-login-register").show();
+			}
+			
+			var $data = $("#mollify-login-data");
+			mollify.ui.handlers.center($data);
+			//mollify.ui.handlers.bubble($data, that);
+			$("#mollify-login-name, #mollify-login-password").bind('keypress', function(e) {
+				if ((e.keyCode || e.which) == 13) that.onLogin();
+			});
+			$("#mollify-login-button").click(that.onLogin);
+			$("#mollify-login-name").focus();
+		}
+		
+		that.onResize = function() {
+			var h = $(window).height();
+			$("#mollify-login-main").height(h);
+			
+			var $data = $("#mollify-login-data");
+			$data.css('margin-top', (h / 2) - ($data.height() / 2));
+		}
+		
+		that.onRenderBubble = function(id, bubble) {
+			if (id === 'mollify-login-forgot-password') {
+				$("#mollify-login-forgot-button").click(function() {				
+					var email = $("#mollify-login-forgot-email").val();
+					if (!email) return;
+					
+					bubble.hide();
+					that.wait = mollify.ui.dialogs.wait({target: "mollify-login-main"});
+					//TODO that.listener.onResetPassword(email);
+				});
+			}
+		}
+		
+		that.onShowBubble = function(id, bubble) {
+			if (id === 'mollify-login-forgot-password') {
+				$("#mollify-login-forgot-email").val("").focus();
+			}
+		}
+		
+		that.onLogin = function() {
+			var username = $("#mollify-login-name").val();
+			var password = $("#mollify-login-password").val();
+			var remember = $("#mollify-login-remember-cb").is(':checked');
+			
+			if (!username || username.length < 1) {
+				$("#mollify-login-name").focus();
+				return;
+			}
+			if (!password || password.length < 1) {
+				$("#mollify-login-password").focus();
+				return;
+			}
+			that.wait = mollify.ui.dialogs.wait({target: "mollify-login-main"});
+			mollify.service.post("session/authenticate", {protocol_version: 3, username: username, password: Base64.encode(password), remember: remember}, function(s) {
+				mollify.App.setSession(s);
+			}, function(c, e) {
+				that.showLoginError();
+			});
+		}
+		
+		that.showLoginError = function() {
+			that.wait.close();
+			
+			mollify.ui.dialogs.notification({
+				message: mollify.ui.texts.get('loginDialogLoginFailedMessage')
+			});
+		}
+		
+		that.onResetPasswordSuccess = function() {
+			that.wait.close();
+			
+			mollify.ui.dialogs.notification({
+				message: mollify.ui.texts.get('resetPasswordPopupResetSuccess')
+			});
+		}
+		
+		that.onResetPasswordFailed = function() {
+			that.wait.close();
+			
+			mollify.ui.dialogs.info({
+				message: mollify.ui.texts.get('resetPasswordPopupResetFailed')
+			});
+		}
+	};
+}(window.jQuery, window.mollify);
---
+++ js/uploader.js
@@ -0,0 +1,168 @@
+!function($, mollify) {
+
+	"use strict"; // jshint ;_;
+
+	mollify.MollifyHTML5Uploader = function() {
+		var t = this;
+		
+		// prevent default file drag&drop		
+		$(document).bind('drop dragover', function (e) {
+		    e.preventDefault();
+		    return false;
+		});
+		
+		this.open = function(folder) {
+			var $d = mollify.dom.template("mollify-tmpl-uploader-dlg");
+			mollify.ui.dialogs.custom({
+				element: $d,
+				"title-key": 'fileUploadDialogTitle',
+				buttons: [
+					{ id:0, "title-key": "upload" },
+					{ id:1, "title-key": "cancel" }
+				],
+				"on-button": function(btn, dlg) {
+					if (btn.id == 1)
+						dlg.close();
+					else t.onUpload($d, dlg,folder);
+				},
+				"on-show": function(dlg) { t.onOpen($d, dlg, folder); }
+			});
+		};
+		
+		this.onOpen = function($d, dlg, folder) {
+			//var $form = $d.find(".mollify-uploader-form");//.attr("action", );
+			var $input = $d.find("input").on('change', function() {
+				//if (!this.files || this.files.length == 0) return;
+				//if (this.files.length == 1) alert(this.files[0].name);
+				//else alert(this.files.length);
+			}).fileupload({
+				url: mollify.service.url("filesystem/"+folder.id+'/files/'),
+				dataType: 'json',
+				dropZone: $d.find(".mollify-uploader").bind("dragover", function(e) { e.stopPropagation(); }),
+			    drop: function (e, data) {
+			        alert('Dropped: ' + data.files.length);
+			    },
+				progressall: function (e, data) {
+					var progress = parseInt(data.loaded / data.total * 100, 10);
+					console.log(progress);
+			    },
+				done: function(e, data) {
+					
+				}
+			});	
+		};
+		
+		this._initDropZoneEffects = function($e) {
+			$e.bind('dragover', function (e) {
+				e.stopPropagation();
+			    var dropZone = $e,
+			        timeout = window.dropZoneTimeout;
+			    if (!timeout) {
+			        dropZone.addClass('in');
+			    } else {
+			        clearTimeout(timeout);
+			    }
+			    if (e.target === dropZone[0]) {
+			        dropZone.addClass('hover');
+			    } else {
+			        dropZone.removeClass('hover');
+			    }
+			    window.dropZoneTimeout = setTimeout(function () {
+			        window.dropZoneTimeout = null;
+			        dropZone.removeClass('in hover');
+			    }, 100);
+			});
+		};
+		
+		this.initWidget = function($e, folder, l) {
+			var $d = mollify.dom.template("mollify-tmpl-uploader-widget");
+			$e.append($d);
+			mollify.ui.handlers.localize($e);
+			var $dropZone = $("#mollify-uploader-widget");
+			
+			var $input = $d.find("input").fileupload({
+				url: mollify.service.url("filesystem/"+folder.id+'/files/'),
+				dataType: 'json',
+				dropZone: $dropZone,
+			    /*add: function (e, data) {
+			    	$input.attr("disabled", "disabled");
+			        //alert('Dropped: ' + data.files.length);
+			        uploadData.push(data);
+			        totalFiles = totalFiles + data.files.length;
+			        console.log(totalFiles);
+			    },*/
+			    /*send: function(e, data) {
+			    	//if (data.files.length == 0) return false;
+				    //if (l.start) l.start(data.files);
+			    },*/
+			    submit: function (e, data) {
+				    var $this = $(this);
+				    if (l.start) l.start(data.files, function() {
+					    $this.fileupload('send', data);
+				    });
+    				return false;
+    			},
+				progressall: function (e, data) {
+					if (!l.progress) return;
+					
+					var progress = parseInt(data.loaded / data.total * 100, 10);
+					l.progress(progress);
+			    },
+				done: function(e, data) {
+					if (l.finished) l.finished();
+				}
+			});
+			
+			t._initDropZoneEffects($dropZone);
+		};
+		
+		return {
+			open : function(folder) {
+				mollify.templates.load("mollify-uploader", mollify.templates.url("uploader.html"), function() {
+					t.open(folder);
+				});
+			},
+			initUploadWidget : function($e, folder, l) {
+				mollify.templates.load("mollify-uploader", mollify.templates.url("uploader.html"), function() {
+					t.initWidget($e, folder, l);
+				});
+			},
+			initMainViewUploader : function(h) {
+				var $p = h.container;
+				var $container = $('<div style="width: 0px; height: 0px"></div>').appendTo($p);
+				var $form = $('<form enctype="multipart/form-data"></form>').appendTo($container);
+				t.$mainViewInput = $('<input type="file" class="mollify-mainview-uploader-input" name="uploader-html5[]" multiple="multiple"></input>').appendTo($form).fileupload({
+					url: '',
+					dataType: 'json',
+					dropZone: h.dropElement,
+				    submit: function (e, data) {
+					    var $this = $(this);
+					    if (h.start) h.start(data.files, function() {
+						    $this.fileupload('send', data);
+					    });
+        				return false;
+        			},
+					progressall: function (e, data) {
+						if (!h.progress) return;
+						
+						var progress = parseInt(data.loaded / data.total * 100, 10);
+						h.progress(progress);
+				    },
+					done: function(e, data) {
+						if (h.finished) h.finished();
+					}
+				}).fileupload('disable');
+				t._initDropZoneEffects(h.dropElement);
+			},
+			setMainViewUploadFolder : function(f) {
+				if (!t.$mainViewInput) return;
+				if (!f) {
+					t.$mainViewInput.fileupload('disable');
+					return;
+				}
+				t.$mainViewInput.fileupload('enable').fileupload('option', 'url', mollify.service.url("filesystem/"+f.id+'/files/'));
+			}
+		};
+	}
+
+}(window.jQuery, window.mollify);
