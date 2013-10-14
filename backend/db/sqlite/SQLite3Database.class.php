96afc9b68929239d354b84c596fb7d0fa56a0958
samuli.jarvela <samuli.jarvela@gmail.com>
2013-10-14 08:41:30 +0000

    sqlite3

---
+++ backend/db/sqlite/SQLite3Database.class.php
@@ -0,0 +1,205 @@
+<?php
+
+	/**
+	 * SQLiteDatabase.class.php
+	 *
+	 * Copyright 2008- Samuli Järvelä
+	 * Released under GPL License.
+	 *
+	 * License: http://www.mollify.org/license.php
+	 */
+
+	class MollifySQLite3Database {
+		private $file;		
+		private $db = NULL;
+		
+		public static function createFromConf($conf) {
+			if (!isset($conf["file"])) throw new ServiceException("INVALID_CONFIGURATION", "No SQLite database file defined");
+			
+			$file = $conf["file"];
+			$db = new MollifySQLite3Database($file);
+			$db->connect();
+			return $db;
+		}
+		
+		public function __construct($file) {
+			Logging::logDebug("SQLite DB: ".$file);
+			$this->file = $file;
+		}
+		
+		public function type() {
+			return "sqlite";
+		}
+		
+		public function databaseExists() {
+			return file_exists($this->file);
+		}
+
+		public function file() {
+			return $this->file;
+		}
+		
+		public function isConnected() {
+			return $this->db != NULL;
+		}
+		
+		public function connect() {
+			$db = new SQLite3($this->file);
+			if (!$db) throw new ServiceException("INVALID_CONFIGURATION", "Could not connect to database (file=".$this->file.")");
+			$this->db = $db;
+			$this->registerRegex();
+		}
+		
+		public function registerRegex() {
+			$this->db->createFunction('REGEX', 'sqlite_regex_match', 2);
+		}
+
+		public function table($name) {
+			return $name;
+		}
+		
+		public function update($query) {
+			$result = $this->query($query);
+			$affected = $result->affected();
+			$result->free();
+			return $affected;
+		}
+
+		public function query($query) {
+			if (Logging::isDebug()) Logging::logDebug("DB: ".$query);
+			
+			$result = $this->db->query(str_replace("`", "", $query));
+			if (!$result)
+				throw new ServiceException("INVALID_CONFIGURATION", "Error executing query (".$query."): ".$this->db->lastErrorMsg());
+			return new Result($this->db, $result);
+		}
+		
+		public function execSqlFile($file) {
+			$sql = file_get_contents($file);
+			if (!$sql) throw new ServiceException("INVALID_REQUEST", "Error reading sql file (".$file.")");
+			$sql = str_replace("{TABLE_PREFIX}", "", str_replace("`", "", $sql));
+			$this->queries($sql);
+		}
+
+		public function queries($query) {
+			if (Logging::isDebug()) Logging::logDebug("DB: ".$query);
+			
+			$result = $this->db->exec($query);
+			if (!$result)
+				throw new ServiceException("INVALID_CONFIGURATION", "Error executing query (".$query."): ".$this->db->lastErrorMsg());
+			return TRUE;
+		}
+		
+		public function startTransaction() {
+			$result = $this->db->exec("BEGIN;");
+			if (!$result)
+				throw new ServiceException("INVALID_CONFIGURATION", "Error starting transaction: ".$this->db->lastErrorMsg());
+		}
+
+		public function commit() {
+			$result = $this->db->exec("COMMIT;");
+			if (!$result)
+				throw new ServiceException("INVALID_CONFIGURATION", "Error committing transaction: ".$this->db->lastErrorMsg());
+		}
+		
+		public function rollback() {
+			$result = $this->db->exec("ROLLBACK;");
+			if (!$result)
+				throw new ServiceException("INVALID_CONFIGURATION", "Error rollbacking transaction: ".$this->db->lastErrorMsg());
+		}
+		
+		public function string($s, $quote = FALSE) {
+			if ($s === NULL) return 'NULL';
+			$r = $this->db->escapeString($s);
+			if ($quote) return "'".$r."'";
+			return $r;
+		}
+		
+		public function arrayString($a, $quote = FALSE) {
+			$result = '';
+			$first = TRUE;
+			foreach($a as $s) {
+				if (!$first) $result .= ',';
+				if ($quote) $result .= "'".$s."'";
+				else $result .= $s;
+				$first = FALSE;
+			}
+			return $result;
+		}
+		
+		public function lastId() {
+			return $this->db->lastInsertRowID();
+		}
+	}
+	
+	class Result {
+		private $db;
+		private $result;
+		
+		public function __construct($db, $result) {
+			$this->db = $db;
+			$this->result = $result;
+		}
+		
+		public function count() {
+			return count($this->rows());
+		}
+
+		public function affected() {
+			return $this->db->changes();
+		}
+				
+		public function rows() {
+			$list = array();
+			while ($row = $this->result->fetchArray(SQLITE3_BOTH)) {
+				$list[] = $row;
+			}
+			return $list;
+		}
+
+		public function values($col) {
+			$list = array();
+			while ($row = $this->result->fetchArray(SQLITE3_ASSOC)) {
+				$list[] = $row[$col];
+			}
+			return $list;
+		}
+		
+		public function firstValue($val) {
+			$ret = $this->firstRow();
+			return $ret[$val];
+		}
+			
+		public function valueMap($keyCol, $valueCol = NULL) {
+			$list = array();
+			while ($row = $this->result->fetchArray(SQLITE3_ASSOC)) {
+				if ($valueCol == NULL)
+					$list[$row[$keyCol]] = $row;
+				else
+					$list[$row[$keyCol]] = $row[$valueCol];
+			}
+			return $list;
+		}
+			
+		public function firstRow() {
+			$ret = $this->result->fetchArray(SQLITE3_ASSOC);
+			return $ret;
+		}
+		
+		public function value($r=0, $f=0) {
+			$rows = $this->rows();
+			$row = $rows[$r];
+			return $row[$f];
+		}
+		
+		public function free() {
+		}
+	}
+	
+	function sqlite_regex_match($str, $regex) {  
+		if (preg_match($regex, $str, $matches)) {  
+			return $matches[0];  
+		}
+		return false;  
+	}
+?>
